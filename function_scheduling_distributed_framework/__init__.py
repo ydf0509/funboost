b78c23b89cee3b17ceb737d7888f08138b4da81b
FQ9H7TVDZLJ4RBT\Administrator <909686719@qq.com>
2019-08-08 21:22:24 +0800

    no message

--- function_scheduling_distributed_framework/__init__.py
+++ function_scheduling_distributed_framework/__init__.py
@@ -1,6 +1,7 @@
 import copy
 
 from function_scheduling_distributed_framework import frame_config
+from function_scheduling_distributed_framework.consumers.base_consumer import ExceptionForRequeue,ExceptionForRetry
 from function_scheduling_distributed_framework.factories.publisher_factotry import get_publisher
 from function_scheduling_distributed_framework.factories.consumer_factory import get_consumer
 from function_scheduling_distributed_framework.utils import nb_print

479e63c1489ca9f4db68bad59f1f982e69f34e6e
FQ9H7TVDZLJ4RBT\Administrator <909686719@qq.com>
2019-08-09 14:22:11 +0800

    no message

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='function_scheduling_distributed_framework',  #
-    version="0.6",
+    version="0.7",
     description=(
         'function scheduling distributed framework'
     ),

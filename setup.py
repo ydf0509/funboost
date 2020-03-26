3d3dee1fa6a2b73c4aace2af30a776d608eebfae
yangdefengredmibook <909686719@qq.com>
2020-03-26 14:55:16 +0800

    no message

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='function_scheduling_distributed_framework',  #
-    version="4.7",
+    version="5.0",
     description=(
         'function scheduling distributed framework'
     ),

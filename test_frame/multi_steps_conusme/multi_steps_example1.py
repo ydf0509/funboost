687ad25765a46b45bbcbea3dda39ef9c24c14f34
yangdefeng <909686719@qq.com>
2020-07-16 10:26:25 +0800

    no message

--- test_frame/multi_steps_conusme/multi_steps_example1.py
+++ test_frame/multi_steps_conusme/multi_steps_example1.py
@@ -1,17 +1,17 @@
-from function_scheduling_distributed_framework import get_consumer, task_deco
+from function_scheduling_distributed_framework import  task_deco
 
 
-@task_deco('queue_test_step1')
+@task_deco('queue_test_step1',qps=0.5)
 def step1(x):
     print(f'x 的值是 {x}')
     if x == 0:
-        for i in range(1, 10):
-            step1.pub(dict(x=x + i))
+        for i in range(1, 30):
+            step2.pub(dict(y=x + i))
     for j in range(10):
-        step2.pub(dict(y=x * 100 + j))
+        step1.pub(dict(x=x * 100 + j))
 
 
-@task_deco('queue_test_step2')
+@task_deco('queue_test_step2',qps=3)
 def step2(y):
     print(f'y 的值是 {y}')
 

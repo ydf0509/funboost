11d1b138e35d74d0161f35d383fa1f99b1663cee
PC-202005110002\Administrator <909686719@qq.com>
2020-10-31 11:34:30 +0800

    1

--- test_frame/best_simple_example/test_consume.py
+++ test_frame/best_simple_example/test_consume.py
@@ -3,12 +3,12 @@
 # @Time    : 2019/8/8 0008 14:57
 import time
 
-from function_scheduling_distributed_framework import task_deco, LogManager
+from function_scheduling_distributed_framework import task_deco, LogManager,BrokerEnum
 
 logger = LogManager('test.f2').get_logger_and_add_handlers(formatter_template=7)
 
 
-@task_deco('queue_test2', qps=50, broker_kind=6)  # 通过设置broker_kind，一键切换中间件为mq或redis等13种中间件或包。
+@task_deco('queue_test2', qps=3, broker_kind=BrokerEnum.PERSISTQUEUE)  # 通过设置broker_kind，一键切换中间件为mq或redis等13种中间件或包。
 def f2(a, b):
     print(f'消费此消息 {a} + {b} 中。。。。。')
     time.sleep(10)  # 模拟做某事需要阻塞10秒种，必须用并发绕过此阻塞。
@@ -17,6 +17,6 @@ def f2(a, b):
 
 if __name__ == '__main__':
     f2.clear()
-    for i in range(10000):
+    for i in range(100):
         f2.push(i, i * 2)
     f2.consume()

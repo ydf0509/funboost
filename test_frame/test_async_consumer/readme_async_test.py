ff3ec2f41a72783ed3f23415cfc70a83ac22c06a
PC-202005110002\Administrator <909686719@qq.com>
2020-12-06 00:14:58 +0800

    no message

---
+++ test_frame/test_async_consumer/readme_async_test.py
@@ -0,0 +1,14 @@
+from function_scheduling_distributed_framework import task_deco, BrokerEnum,ConcurrentModeEnum
+import asyncio
+
+@task_deco('test_async_queue2', concurrent_mode=ConcurrentModeEnum.ASYNC,
+            broker_kind=BrokerEnum.LOCAL_PYTHON_QUEUE, log_level=10,concurrent_num=500,)
+async def async_f(x):
+    # await asyncio.sleep(0.1,)
+    print(x)
+
+if __name__ == '__main__':
+    async_f.clear()
+    for i in range(50000):
+        async_f.push(i, )
+    async_f.consume()

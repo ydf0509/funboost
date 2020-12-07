06a7790c1dfd78bf5b981cb40bb42c2fe1a48262
yangdefeng <909686719@qq.com>
2020-12-07 11:29:05 +0800

    no message

--- function_scheduling_distributed_framework/concurrent_pool/async_pool_executor.py
+++ function_scheduling_distributed_framework/concurrent_pool/async_pool_executor.py
@@ -24,11 +24,8 @@ class AsyncPoolExecutor2:
         Thread(target=self._start_loop_in_new_thread).start()
 
     def submit(self, func, *args, **kwargs):
-        while True:  # 阻止过快放入。
-            if self._sem.locked():
-                time.sleep(0.01)
-            else:
-                break
+        while self._sem.locked():
+            time.sleep(0.01)
         asyncio.run_coroutine_threadsafe(self._run_func(func, *args, **kwargs), self.loop)
 
     async def _run_func(self, func, *args, **kwargs):
@@ -113,25 +110,112 @@ class AsyncPoolExecutor:
         print('关闭循环')
 
 
+class AsyncProducerConsumer:
+    """
+    参考 https://asyncio.readthedocs.io/en/latest/producer_consumer.html 官方文档。
+    A simple producer/consumer example, using an asyncio.Queue:
+    """
+
+    """
+    按时间间隔慢慢添加任务，边生产边消费。此框架没用到这个类，这个要求生产和消费在同一个线程里面，不方便。
+    """
+
+    def __init__(self, items, concurrent_num=200, consume_fun_specify=None):
+        """
+
+        :param items: 要消费的参数列表
+        :param time_interval_produce: 添加任务的时间间隔
+        :param consume_fun_specify: 指定的异步消费函数对象，如果不指定就要继承并重写consume_fun函数。
+        """
+        self.queue = asyncio.Queue()
+        self.items = items
+        self._concurrent_num = concurrent_num
+        self.consume_fun_specify = consume_fun_specify
+
+    async def produce(self):
+        for item in self.items:
+            await self.queue.put(item)
+
+    async def consume(self):
+        while True:
+            # wait for an item from the producer
+            item = await self.queue.get()
+            # process the item
+            # print('consuming {}...'.format(item))
+            # simulate i/o operation using sleep
+            try:
+                if self.consume_fun_specify:
+                    await self.consume_fun_specify(item)
+                else:
+                    await self.consume_fun(item)
+            except Exception as e:
+                print(e)
+
+            # Notify the queue that the item has been processed
+            self.queue.task_done()
+
+    @staticmethod
+    async def consume_fun(item):
+        """
+        要么继承此类重写此方法，要么在类的初始化时候指定consume_fun_specify为一个异步函数。
+        :param item:
+        :return:
+        """
+        print(item, '请重写 consume_fun 方法')
+        await asyncio.sleep(1)
+
+    async def __run(self):
+        # schedule the consumer
+        tasks = []
+        for _ in range(self._concurrent_num):
+            task = asyncio.ensure_future(self.consume())
+            tasks.append(task)
+        # run the producer and wait for completion
+        await self.produce()
+        # wait until the consumer has processed all items
+        await self.queue.join()
+        # the consumer is still awaiting for an item, cancel it
+        for task in tasks:
+            task.cancel()
+
+    def start_run(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.__run())
+        # loop.close()
+
+
 if __name__ == '__main__':
-    from function_scheduling_distributed_framework.concurrent_pool.bounded_threadpoolexcutor import ThreadPoolExecutor
+    def test_async_pool_executor():
+        from function_scheduling_distributed_framework.concurrent_pool.bounded_threadpoolexcutor import ThreadPoolExecutor
+
+        async def f(x):
+            # await asyncio.sleep(1)
+            # raise Exception('aaa')
+            print('打印', x)
+
+        print(1111)
+        pool = AsyncPoolExecutor(500)
+        # pool = ThreadPoolExecutor(500)  # 协程不能用线程池运行，否则压根不会执行print打印，对于一部函数 f(x)得到的是一个协程，必须进一步把协程编排成任务放在loop循环里面运行。
+        for i in range(1, 41001):
+            print('放入', i)
+            pool.submit(f, i)
+        time.sleep(5)
+        pool.submit(f, 'hi')
+        pool.submit(f, 'hi2')
+        pool.submit(f, 'hi3')
+        print(2222)
+        # pool.shutdown()
 
 
-    async def f(x):
-        # await asyncio.sleep(1)
-        # raise Exception('aaa')
-        print('打印', x)
+    async def _my_fun(item):
+        print('嘻嘻', item)
+        await asyncio.sleep(1)
 
 
-    print(1111)
-    pool = AsyncPoolExecutor(500)
-    # pool = ThreadPoolExecutor(500)  # 协程不能用线程池运行，否则压根不会执行print打印，对于一部函数 f(x)得到的是一个协程，必须进一步把协程编排成任务放在loop循环里面运行。
-    for i in range(1, 41001):
-        print('放入', i)
-        pool.submit(f, i)
-    time.sleep(5)
-    pool.submit(f, 'hi')
-    pool.submit(f, 'hi2')
-    pool.submit(f, 'hi3')
-    print(2222)
-    # pool.shutdown()
+    def test_async_producer_consumer():
+        AsyncProducerConsumer([i for i in range(1000)], concurrent_num=200, consume_fun_specify=_my_fun).start_run()
+        print('over')
+
+
+    test_async_pool_executor()
+    # test_async_producer_consumer()

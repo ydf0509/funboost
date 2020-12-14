780dc6b943884ebf62917ee9c88e9383e7d5a7e7
yangdefeng <909686719@qq.com>
2020-12-14 15:27:58 +0800

    no message

--- function_scheduling_distributed_framework/set_frame_config.py
+++ function_scheduling_distributed_framework/set_frame_config.py
@@ -74,6 +74,9 @@ def show_frame_config():
 
 config_file_content = '''# -*- coding: utf-8 -*-
 """
+此文件是分布式函数调度框架第一次运行时候自动生成的，按需修改里面的配置项。
+比如你只用redis，把redis前面的 # 注释放开，修改成你的值，无需理会其他配置项如 rabbitmq  kafka。
+
 1.1） 此py配置文件的内容是可以不拘于形式的，例如在此文件里面可以根据环境写if else 判断，或者写其他python代码，里面写各种函数和变量都可以的。但模块必须按需包括以下名字的变量，
 例如 如果不使用rabbitmq作为中间件，只使用redis做中间件，则此模块可以没有RABBITMQ_USER RABBITMQ_PASS MOOGO KAFKA sql数据库等这些变量，只需要redis相关的变量就可以。
 

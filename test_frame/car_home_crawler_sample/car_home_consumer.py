e0f420e0f48f6d54552f94ba9b75e07a2e657eea
ydf0509 <ydf0509@qq.com>
2021-04-10 23:13:40 +0800

    commit

--- test_frame/car_home_crawler_sample/car_home_consumer.py
+++ test_frame/car_home_crawler_sample/car_home_consumer.py
@@ -3,7 +3,7 @@ from parsel import Selector
 from function_scheduling_distributed_framework import task_deco, BrokerEnum, run_consumer_with_multi_process
 
 
-@task_deco('car_home_list', broker_kind=BrokerEnum.REDIS_ACK_ABLE, max_retry_times=5, qps=0.5)
+@task_deco('car_home_list', broker_kind=BrokerEnum.REDIS_ACK_ABLE, max_retry_times=5, qps=2)
 def crawl_list_page(news_type, page, do_page_turning=False):
     url = f'https://www.autohome.com.cn/{news_type}/{page}/#liststart'
     resp_text = requests.get(url).text
@@ -19,7 +19,7 @@ def crawl_list_page(news_type, page, do_page_turning=False):
             crawl_list_page.push(news_type, p)  # 列表页翻页。
 
 
-@task_deco('car_home_detail', broker_kind=BrokerEnum.REDIS_ACK_ABLE, concurrent_num=600, qps=2, do_task_filtering=True)
+@task_deco('car_home_detail', broker_kind=BrokerEnum.REDIS_ACK_ABLE, concurrent_num=600, qps=5, do_task_filtering=True)
 def crawl_detail_page(url, title, news_type):
     resp_text = requests.get(url).text
     sel = Selector(resp_text)

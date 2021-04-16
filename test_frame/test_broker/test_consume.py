c9c8d558da2609b37e8bc5880037f8cada1559f8
yangdefeng <909686719@qq.com>
2021-04-16 09:56:28 +0800

    no message

--- test_frame/test_broker/test_consume.py
+++ test_frame/test_broker/test_consume.py
@@ -1,10 +1,11 @@
 import time
 from function_scheduling_distributed_framework import task_deco,BrokerEnum
 
-@task_deco('30778',broker_kind=BrokerEnum.REDIS_ACK_ABLE,qps=5,)
+@task_deco('test_confirm_queue',broker_kind=BrokerEnum.REDIS_ACK_ABLE,qps=10,)
 def f(x):
-    time.sleep(3)
-    print(x)
+    print(f' 待消费 {x}')
+    time.sleep(30)
+    print(f'消费完成 {x}')
 
 
 if __name__ == '__main__':

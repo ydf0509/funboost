917c973a9f7b041740e18cee6b028268739a17a8
ydf0509 <ydf0509@qq.com>
2021-04-21 01:01:15 +0800

    commit

--- function_scheduling_distributed_framework/publishers/redis_publisher.py
+++ function_scheduling_distributed_framework/publishers/redis_publisher.py
@@ -34,7 +34,7 @@ class RedisPublisher(AbstractPublisher, RedisMixin):
 
     def concrete_realization_of_publish(self, msg):
         # print(getattr(frame_config,'has_start_a_consumer',0))
-        # 这里的 has_start_a_consumer 是一个标志，附着在此模块的一个标识变量而已，框架自动设定的，不要把这个变量写到模块里面。
+        # 这里的 has_start_a_consumer 是一个标志，借用此模块设置的一个标识变量而已，框架运行时候自动设定的，不要把这个变量写到模块里面。
         if getattr(frame_config, 'has_start_a_consumer', 0) == 0:  # 加快速度推送，否则每秒只能推送4000次。如果是独立脚本推送，使用批量推送，如果是消费者中发布任务，为了保持原子性，用原来的单个推送。
             # self._temp_msg_queue.put(msg)
             with self._lock_for_bulk_push:

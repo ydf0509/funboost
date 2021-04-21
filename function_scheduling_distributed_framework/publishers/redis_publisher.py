26d7cffa4c2078e8eed5a7e2af8305e2c21473c0
yangdefeng <909686719@qq.com>
2021-04-21 19:45:41 +0800

    no message

--- function_scheduling_distributed_framework/publishers/redis_publisher.py
+++ function_scheduling_distributed_framework/publishers/redis_publisher.py
@@ -51,9 +51,8 @@ class RedisPublisher(AbstractPublisher, RedisMixin):
     def clear(self):
         self.redis_db_frame.delete(self._queue_name)
         # self.redis_db_frame.delete(f'{self._queue_name}__unack')
-
         unack_queue_name_list = self.redis_db_frame.scan(match=f'{self._queue_name}__unack_id_*', count=10000)[1] + \
-                                self.redis_db_frame.scan(match=f'unack_{self._queue_name}_*', count=10000)[1]
+                                self.redis_db_frame.scan(match=f'unack_{self._queue_name}_*', count=10000)[1]  # noqa
         self.redis_db_frame.delete(*unack_queue_name_list)
         self.logger.warning(f'清除 {self._queue_name} 队列中的消息成功')
         if unack_queue_name_list:

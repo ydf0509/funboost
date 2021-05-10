7abdfbe4479d1caaef0cd2afa2dbd258bf99b6b6
ydf0509 <ydf0509@qq.com>
2021-05-10 21:39:53 +0800

    commit

---
+++ test_frame/git_update.py
@@ -0,0 +1,19 @@
+import subprocess
+
+print(subprocess.getstatusoutput('git pull'))
+
+
+print(subprocess.getstatusoutput('git add ../.'))
+
+print(subprocess.getstatusoutput('git commit -m commit'))
+
+
+print(subprocess.getstatusoutput('git push origin'))
+
+# print(subprocess.getstatusoutput('git push github'))
+
+
+
+
+
+

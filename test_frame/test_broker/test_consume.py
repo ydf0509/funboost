f6e38cec93656ce2e434eeb63e3ed11697b0592f
yangdefeng <909686719@qq.com>
2021-06-15 16:00:04 +0800

    commit

--- test_frame/test_broker/test_consume.py
+++ test_frame/test_broker/test_consume.py
@@ -3,7 +3,7 @@ import random
 from function_scheduling_distributed_framework import task_deco, BrokerEnum
 
 
-@task_deco('test_queue66', broker_kind=BrokerEnum.PERSISTQUEUE, qps=100, log_level=10, is_print_detail_exception=False, is_show_message_get_from_broker=False,
+@task_deco('test_queue66', broker_kind=BrokerEnum.RABBITMQ_AMQPSTORM, qps=100, log_level=10, is_print_detail_exception=False, is_show_message_get_from_broker=False,
            is_using_distributed_frequency_control=True)
 def f(x, y):
     # time.sleep(10)

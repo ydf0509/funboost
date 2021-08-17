565be2f3e03464e39b62c9078ea96fd2ef785df2
yangdefeng <909686719@qq.com>
2021-08-17 16:46:26 +0800

    no message

--- function_scheduling_distributed_framework/consumers/kafka_consumer_manually_commit.py
+++ function_scheduling_distributed_framework/consumers/kafka_consumer_manually_commit.py
@@ -20,7 +20,8 @@ from nb_log import LogManager
 class KafkaConsumerManuallyCommit(AbstractConsumer):
     """
     confluent_kafla作为中间件实现的。操作kafka中间件的速度比kafka-python快10倍。
-    这个是手动确认，由于是异步在并发池中并发消费，可以防止强制关闭程序造成正在运行的任务丢失，比自动commit好。
+    这个是自动间隔2秒的手动确认，由于是异步在并发池中并发消费，可以防止强制关闭程序造成正在运行的任务丢失，比自动commit好。
+    如果使用kafka，推荐这个。
     """
     BROKER_KIND = 16
 

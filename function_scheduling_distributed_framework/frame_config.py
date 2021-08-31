6fc986751b74847fb077162bb9ddde474bf132a1
yangdefeng <909686719@qq.com>
2021-08-31 11:43:08 +0800

    commit

--- function_scheduling_distributed_framework/frame_config.py
+++ function_scheduling_distributed_framework/frame_config.py
@@ -5,7 +5,16 @@ from function_scheduling_distributed_framework.constant import BrokerEnum
 '''
 你项目根目录下自动生成的 distributed_frame_config.py 文件中修改配置，会被自动读取到。
 此文件按需修改，例如你使用redis中间件作为消息队列，可以不用管rabbitmq的配置。
+
+框架使用文档是 https://function-scheduling-distributed-framework.readthedocs.io/zh_CN/latest/
+文档文字比celery少了50倍，要是这么短和简单的都不想看，那就更不可能耐心看celery文档。
 '''
+
+# 如果@task_deco装饰器没有亲自指定beoker_kind入参，则默认使用DEFAULT_BROKER_KIND这个中间件。
+# 强烈推荐安装rabbitmq然后使用 BrokerEnum.RABBITMQ_AMQPSTORM 这个中间件,
+# 次之 BrokerEnum.REDIS_ACK_ABLE中间件，kafka则推荐 BrokerEnum.CONFLUENT_KAFKA。
+# BrokerEnum.PERSISTQUEUE 的优点是基于单机磁盘的消息持久化，不需要安装消息中间件软件就能使用，但不是跨机器的真分布式。
+DEFAULT_BROKER_KIND = BrokerEnum.PERSISTQUEUE
 
 MONGO_CONNECT_URL = f'mongodb://myUserAdmin:8mwTdy1klnSYepNo@{"192.168.199.202"}:27016/admin'
 
@@ -43,10 +52,6 @@ HTTPSQS_AUTH = '123456'
 KOMBU_URL = 'redis://127.0.0.1:6379/0'
 # KOMBU_URL =  'sqla+sqlite:////dssf_kombu_sqlite.sqlite'  # 4个//// 代表磁盘根目录下生成一个文件。推荐绝对路径。3个///是相对路径。
 
-# 如果@task_deco装饰器没有亲自指定beoker_kind入参，则默认使用DEFAULT_BROKER_KIND这个中间件。
-# 强烈推荐安装rabbitmq然后使用 BrokerEnum.RABBITMQ_AMQPSTORM 这个中间件,次之 BrokerEnum.REDIS_ACK_ABLE中间件，kafka则推荐 BrokerEnum.CONFLUENT_KAFKA。
-# BrokerEnum.PERSISTQUEUE 的优点是基于单机磁盘的消息持久化，不需要安装消息中间件软件就能使用，但不是真跨机器的分布式。
-DEFAULT_BROKER_KIND = BrokerEnum.PERSISTQUEUE
 
 # nb_log包的第几个日志模板，内置了7个模板，可以在你当前项目根目录下的nb_log_config.py文件扩展模板。
 NB_LOG_FORMATER_INDEX_FOR_CONSUMER_AND_PUBLISHER = 7  # 7是简短的不可跳转，5是可点击跳转的

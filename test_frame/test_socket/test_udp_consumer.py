875a7c839e3ef735262b9c03b7ab2c1b902da832
yangdefeng <909686719@qq.com>
2021-10-08 14:37:30 +0800

    commit

--- test_frame/test_socket/test_udp_consumer.py
+++ test_frame/test_socket/test_udp_consumer.py
@@ -1,12 +1,12 @@
 from function_scheduling_distributed_framework import task_deco,BrokerEnum
 
 
-@task_deco('127.0.0.1:5690',broker_kind=BrokerEnum.TCP,log_level=20)
+@task_deco('127.0.0.1:5690',broker_kind=BrokerEnum.TCP)
 def f(x):
     print(x)
 
 
 if __name__ == '__main__':
     f.consume()
-    for i in range(10):
-        f.push(f'hello {i}')
+    # for i in range(10):
+    #     f.push(f'hello {i}')
--- test_frame/test_socket/test_udp_publisher.py
+++ test_frame/test_socket/test_udp_publisher.py
@@ -2,6 +2,6 @@ import time
 from test_udp_consumer import f
 
 for i in range(1000000):
-    # time.sleep(0.1)
+    time.sleep(0.1)
     # print(i)
     f.push(i)

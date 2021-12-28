1f8faa08e4ed5898dc91e82221e0a3f2b1cb73b2
SC-202112251857\Administrator <ydf0509@sohu.com>
2021-12-28 22:16:35 +0800

    commit

--- test_frame/test_get_active_consumer_process_info/test_get_active_consumer_process_info.py
+++ test_frame/test_get_active_consumer_process_info/test_get_active_consumer_process_info.py
@@ -1,7 +1,7 @@
 from funboost import ActiveCousumerProcessInfoGetter
 
 
-
+#test_queue 获取分布式环境中 test_queue 队列的所有消费者信息
 print(ActiveCousumerProcessInfoGetter().get_all_hearbeat_info_by_queue_name('test_queue'))
 print(ActiveCousumerProcessInfoGetter().get_all_hearbeat_info_by_ip())
 print(ActiveCousumerProcessInfoGetter().get_all_hearbeat_info_by_ip('10.0.195.220'))

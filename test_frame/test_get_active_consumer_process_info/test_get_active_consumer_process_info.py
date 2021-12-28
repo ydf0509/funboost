0d7debb760019b9437df0b477fec1af8764bd3db
SC-202112251857\Administrator <ydf0509@sohu.com>
2021-12-28 22:11:20 +0800

    commit

--- test_frame/test_get_active_consumer_process_info/test_get_active_consumer_process_info.py
+++ test_frame/test_get_active_consumer_process_info/test_get_active_consumer_process_info.py
@@ -1,8 +1,8 @@
 from funboost import ActiveCousumerProcessInfoGetter
-from test_frame.my.test_consume import f2
 
 
-print(ActiveCousumerProcessInfoGetter().get_all_hearbeat_info_by_queue_name(f2.queue_name))
+
+print(ActiveCousumerProcessInfoGetter().get_all_hearbeat_info_by_queue_name('test_queue'))
 print(ActiveCousumerProcessInfoGetter().get_all_hearbeat_info_by_ip())
 print(ActiveCousumerProcessInfoGetter().get_all_hearbeat_info_by_ip('10.0.195.220'))
 

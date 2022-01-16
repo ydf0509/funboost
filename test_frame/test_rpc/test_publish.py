6bf65b00f0bc9a1b37077080a8d170e33e5c05a6
SC-202112251857\Administrator <ydf0509@sohu.com>
2022-01-16 21:04:30 +0800

    commit

--- test_frame/test_rpc/test_publish.py
+++ test_frame/test_rpc/test_publish.py
@@ -13,5 +13,5 @@ for i in range(1,50):
     # async_result = add.push(i, i * 2)
     # print(async_result.result)
     async_result = add.publish(dict(a=i*10, b=i * 20),priority_control_config = PriorityConsumingControlConfig(is_using_rpc_mode=True))
-    async_result.set_callback(show_result)
-    # print(async_result.status_and_result)
+    # async_result.set_callback(show_result)
+    print(async_result.status_and_result)

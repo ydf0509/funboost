690f91155fd0afdc22973722b4c747249e52e07d
SC-202112251857\Administrator <ydf0509@sohu.com>
2022-01-16 21:31:27 +0800

    commit

--- funboost/utils/simple_data_class.py
+++ funboost/utils/simple_data_class.py
@@ -1,10 +1,13 @@
 import copy
 from funboost.utils import un_strict_json_dumps
+
+
 class DataClassBase:
     """
     使用类实现的 简单数据类。
     也可以使用装饰器来实现数据类
     """
+
     def __new__(cls, **kwargs):
         self = super().__new__(cls)
         self.__dict__ = copy.copy({k: v for k, v in cls.__dict__.items() if not k.startswith('__')})
@@ -24,20 +27,21 @@ class DataClassBase:
         return f"{self.__class__}    {self.get_dict()}"
 
     def __getitem__(self, item):
-        return getattr(self,item)
+        return getattr(self, item)
 
     def get_json(self):
         un_strict_json_dumps.dict2json(self.get_dict())
 
 
-
 if __name__ == '__main__':
     import datetime
 
+
     class A(DataClassBase):
-        x =1
+        x = 1
         y = 2
         z = datetime.datetime.now()
+
 
     print(A())
     print(A(y=3))

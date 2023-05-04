e4d14d30e69f461712d486b0a122841a3123e955
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-05-04 21:26:34 +0800

    commit

--- funboost/factories/consumer_factory.py
+++ funboost/factories/consumer_factory.py
@@ -3,7 +3,6 @@
 # @Time    : 2022/8/8 0008 13:19
 
 from funboost.consumers.base_consumer import AbstractConsumer
-from funboost.factories.broker_kind__publsiher_consumer_type_map import broker_kind__publsiher_consumer_type_map
 
 
 def get_consumer(*args, broker_kind: int = None, **kwargs) -> AbstractConsumer:
@@ -13,7 +12,7 @@ def get_consumer(*args, broker_kind: int = None, **kwargs) -> AbstractConsumer:
     :param kwargs:
     :return:
     """
-
+    from funboost.factories.broker_kind__publsiher_consumer_type_map import broker_kind__publsiher_consumer_type_map
     if broker_kind not in broker_kind__publsiher_consumer_type_map:
         raise ValueError(f'设置的中间件种类数字不正确,你设置的值是 {broker_kind} ')
     return broker_kind__publsiher_consumer_type_map[broker_kind][1](*args, **kwargs)

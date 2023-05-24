f8927c529ccc1f543485879cfd8db818eefcea8b
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-05-24 19:19:37 +0800

    commit

--- test_frame/test_huey/test_funboost_huey.py
+++ test_frame/test_huey/test_funboost_huey.py
@@ -1,22 +1,23 @@
 import time
 
 from funboost.assist.huey_helper import HueyHelper
-from funboost import boost,BrokerEnum
+from funboost import boost, BrokerEnum
 
-@boost('test_huey_queue1',broker_kind=BrokerEnum.HUEY,)
-def f1(x,y):
+
+@boost('test_huey_queue1', broker_kind=BrokerEnum.HUEY, broker_exclusive_config={'huey_task_kwargs': {}})
+def f1(x, y):
     time.sleep(10)
-    print(x,y)
+    print(x, y)
 
-@boost('test_huey_queue2',broker_kind=BrokerEnum.HUEY)
+
+@boost('test_huey_queue2', broker_kind=BrokerEnum.HUEY)
 def f2(a):
     time.sleep(7)
     print(a)
 
 
-
 if __name__ == '__main__':
     for i in range(10):
-        f1.push(i,i+1)
+        f1.push(i, i + 1)
         f2.push(i)
     HueyHelper.realy_start_huey_consume()

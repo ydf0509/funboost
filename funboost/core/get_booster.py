1a90542dfe84c10823ad69e529de86cfd482f776
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-06-05 19:36:51 +0800

    commit

--- funboost/core/get_booster.py
+++ funboost/core/get_booster.py
@@ -19,7 +19,7 @@ def get_booster_ignore_current_pid(queue_name: str) -> Booster:
     _booster = get_booster_ignore_current_pid(queue_name)
     booster_current_pid = boost(**_booster.boost_params)(_booster.consuming_function)
 
-    这个函数是为了在别的进程实例化 booster，consumer和publisher,获取queue_name队列对应的booster的当时的入参。
+    这个函数是为了在别的进程实例化 booster，consumer和publisher,是为了获取queue_name队列对应的booster的当时的入参。
     有些中间件python包的对中间件连接对象不是多进程安全的，不要在进程2中去操作进程1中生成的booster consumer publisher等对象。
     """
     for k, v in pid_queue_name__booster_map.items():

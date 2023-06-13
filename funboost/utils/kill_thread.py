be72cc7894617c1861c1e2e321464eb25fdbfeb3
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-06-13 09:20:13 +0800

    commit

--- funboost/utils/kill_thread.py
+++ funboost/utils/kill_thread.py
@@ -9,8 +9,24 @@ def kill_thread(threadx:threading.Thread):
 
 def kill_fun_deco(fun,*a,**k):
     def _inner(f):
-        def __inner(*args,**kwargs):
-            return f(*args,**kwargs)
+        def _new_func(oldfunc, result, oldfunc_args, oldfunc_kwargs):
+            result.append(oldfunc(*oldfunc_args, **oldfunc_kwargs))
+
+        def __inner(*args, **kwargs):
+            result = []
+            new_kwargs = {
+                'oldfunc': f,
+                'result': result,
+                'oldfunc_args': args,
+                'oldfunc_kwargs': kwargs
+            }
+
+            thd = threading.Thread(target=_new_func, args=(), kwargs=new_kwargs)
+            thd.start()
+            return result[0]
+
+
+
         return __inner
     return _inner
 

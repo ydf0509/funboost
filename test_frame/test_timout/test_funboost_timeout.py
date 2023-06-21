607195ecc6a2be23b8946effa17dfa54a5366216
SC-202202121439\Administrator <909686719@qq.com>
2023-06-21 21:54:48 +0800

    commit

--- test_frame/test_timout/test_funboost_timeout.py
+++ test_frame/test_timout/test_funboost_timeout.py
@@ -4,15 +4,27 @@ import time
 
 from funboost import boost
 
-@boost('test_timeout_queue',function_timeout=2)
+import threading
+
+lock = threading.Lock()
+
+@boost('test_timeout_queue',function_timeout=10,)
 def f(x):
-    t1 =time.perf_counter()
-    sum =0
-    for i in range(1,10000*10000):
-        sum+=i
-    print(time.perf_counter()-t1)
+    # t1 =time.perf_counter()
+    # sum =0
+    # for i in range(1,10000*10000):
+    #     sum+=i
+    # print(time.perf_counter()-t1)
+    time.sleep(x)
+    lock.acquire()
+    print(f'start {x}')
+    time.sleep(60)
+    print(f'release lock {x}')
+    lock.release()
+    print(f'over {x}')
 
 if __name__ == '__main__':
-    for i in range(1):
-        f.push(1)
+    f.push(7)
+    f.push(20)
+
     f.consume()

706a6806b4621718fb96af576c048953fca6d994
SC-202202121439\Administrator <909686719@qq.com>
2023-06-22 20:48:38 +0800

    commit

--- funboost/utils/expire_lock.py
+++ funboost/utils/expire_lock.py
@@ -1,38 +1,47 @@
+'''
+基于程序内存的过期锁。
+'''
+
 import copy
 from threading import Thread, Event, Lock
 import time
 import typing
 import uuid
 
+from funboost.utils import time_util
 from nb_log import get_logger
 
 lock_key__event_is_free_map: typing.Dict[str, Event] = {}
 
 
 class LockStore:
-    lock0 = Lock()
+    lock_for_operate_store = Lock()
 
-    lock_key__info_map = {}
+    lock_key__info_map: typing.Dict[str, typing.Dict] = {}
 
-    has_start_delete_expire_lock_key_thread = False
+    _has_start_delete_expire_lock_key_thread = False
+
+    THREAD_DELETE_DAEMON = False
+    DELETE_INTERVAL = 0.01
 
     logger = get_logger('LockStore')
 
     @classmethod
-    def _delete_expire_lock_key_thread(cls):
+    def _auto_delete_expire_lock_key_thread(cls):
         while 1:
-            with cls.lock0:
+            with cls.lock_for_operate_store:
                 lock_key__info_map_copy = copy.copy(cls.lock_key__info_map)
                 for lock_key, info in lock_key__info_map_copy.items():
                     if time.time() - info['set_time'] > info['ex']:
                         cls.lock_key__info_map.pop(lock_key)
+                        cls.logger.warning(f'''自动删除占用耗时长的锁 {lock_key} {time_util.DatetimeConverter(info['set_time'])} {info['ex']}''')
                         lock_key__event_is_free_map[lock_key].set()
-            time.sleep(0.01)
+            time.sleep(cls.DELETE_INTERVAL)
 
     @classmethod
     def set(cls, lock_key, value, ex):
         set_succ = False
-        with cls.lock0:
+        with cls.lock_for_operate_store:
             if lock_key not in cls.lock_key__info_map:
                 cls.lock_key__info_map[lock_key] = {'value': value, 'ex': ex, 'set_time': time.time()}
                 set_succ = True
@@ -41,15 +50,15 @@ class LockStore:
                 event_is_free.set()
                 lock_key__event_is_free_map[lock_key] = event_is_free
 
-            if cls.has_start_delete_expire_lock_key_thread is False:
-                cls.has_start_delete_expire_lock_key_thread = True
-                Thread(target=cls._delete_expire_lock_key_thread).start()
+            if cls._has_start_delete_expire_lock_key_thread is False:
+                cls._has_start_delete_expire_lock_key_thread = True
+                Thread(target=cls._auto_delete_expire_lock_key_thread, daemon=cls.THREAD_DELETE_DAEMON).start()
 
         return set_succ
 
     @classmethod
     def delete(cls, lock_key, value):
-        with cls.lock0:
+        with cls.lock_for_operate_store:
             if lock_key in cls.lock_key__info_map:
                 if cls.lock_key__info_map[lock_key]['value'] == value:
                     cls.lock_key__info_map.pop(lock_key)
@@ -59,20 +68,20 @@ class LockStore:
             return False
 
 
-class LockExpireConf:
-    def __init__(self, lock_key, expire_seconds=30):
-        self.lock_key = lock_key
+class ExpireLockConf:
+    def __init__(self, expire_seconds=30, lock_key=None, ):
         self.expire_seconds = expire_seconds
+        self.lock_key = lock_key or uuid.uuid4().hex
 
 
-class ThreadLockExpireAbleContextManager:
+class ExpireLockContextManager:
     """
     分布式redis锁上下文管理.
     """
 
-    def __init__(self, lock_expire_conf: LockExpireConf):
-        self.lock_key = lock_expire_conf.lock_key
+    def __init__(self, lock_expire_conf: ExpireLockConf):
         self.expire_seconds = lock_expire_conf.expire_seconds
+        self.lock_key = lock_expire_conf.lock_key
         self.identifier = str(uuid.uuid4())
         self.has_aquire_lock = False
 
@@ -106,11 +115,12 @@ class ThreadLockExpireAbleContextManager:
 
 
 if __name__ == '__main__':
-    lock_expire = LockExpireConf(lock_key='test_lock_name', expire_seconds=2)
+
+    lockx1_expire = ExpireLockConf(expire_seconds=4, lock_key='test_lock_name_expire', )
 
 
     def f(x):
-        with ThreadLockExpireAbleContextManager(lock_expire):
+        with ExpireLockContextManager(lockx1_expire):
             print(x, time.time())
             time.sleep(5)
 
@@ -120,7 +130,7 @@ if __name__ == '__main__':
 
     def test_raw_lock_fun(x):
         try:
-            test_raw_lock.acquire(timeout=2)
+            test_raw_lock.acquire(timeout=4)
             print(x, time.time())
             time.sleep(5)
             test_raw_lock.release()

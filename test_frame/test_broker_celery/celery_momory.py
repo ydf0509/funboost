9afb6f04802b32caa11c1b4b141be25b2dc82931
ydf0509 <909686719@qq.com>
2023-10-30 22:20:05 +0800

    commit

--- test_frame/test_broker_celery/celery_momory.py
+++ test_frame/test_broker_celery/celery_momory.py
@@ -3,20 +3,19 @@ import time
 from funboost import boost, BrokerEnum, ConcurrentModeEnum, BrokerConnConfig
 from funboost.assist.celery_helper import celery_app, CeleryHelper
 
-BrokerConnConfig.CELERY_BROKER_URL = 'redis://'
-
 
 @boost('test_list_queue', broker_kind=BrokerEnum.CELERY, qps=2, log_level=20, concurrent_mode=ConcurrentModeEnum.SINGLE_THREAD, concurrent_num=1,
        )
 def f(x):
     time.sleep(1)
     print(x)
+    return x*2
 
 
 if __name__ == '__main__':
     f.consume()
-
-    for i in range(1000):
+    f.clear()
+    for i in range(500):
         f.push(i)
     print(f.publisher.get_message_count())
     CeleryHelper.start_flower()

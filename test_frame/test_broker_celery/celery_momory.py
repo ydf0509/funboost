8e3d5d19f765ffe8c6d628b220886c3c5fe813b3
ydf0509 <909686719@qq.com>
2023-10-30 21:54:02 +0800

    commit

--- test_frame/test_broker_celery/celery_momory.py
+++ test_frame/test_broker_celery/celery_momory.py
@@ -3,7 +3,7 @@ import time
 from funboost import boost, BrokerEnum, ConcurrentModeEnum, BrokerConnConfig
 from funboost.assist.celery_helper import celery_app, CeleryHelper
 
-# BrokerConnConfig.CELERY_BROKER_URL = 'redis://'
+BrokerConnConfig.CELERY_BROKER_URL = 'redis://'
 
 
 @boost('test_list_queue', broker_kind=BrokerEnum.CELERY, qps=2, log_level=20, concurrent_mode=ConcurrentModeEnum.SINGLE_THREAD, concurrent_num=1,

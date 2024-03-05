bef80c74fbc9033589e081d4593f9aeeb863adef
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-03-05 18:34:27 +0800

    commit

--- funboost/__init__.py
+++ funboost/__init__.py
@@ -57,6 +57,9 @@ from funboost.utils.ctrl_c_end import ctrl_c_recv
 from funboost.utils.redis_manager import RedisMixin
 from funboost.concurrent_pool.custom_threadpool_executor import show_current_threads_num
 
+from funboost.core.current_task import funboost_current_task
+
+
 # atexit.register(ctrl_c_recv)  # 还是需要用户自己在代码末尾加才可以.
 # set_interrupt_signal_handler()
 
--- funboost/core/current_task.py
+++ funboost/core/current_task.py
@@ -5,6 +5,50 @@ from funboost.core.function_result_status_saver import FunctionResultStatus
 
 
 class ThreadCurrentTask:
+    """
+    用于在用户自己函数内部去获取 消息的完整体,当前重试次数等.
+    """
+    """ 用法例子 
+    '''
+    fct = funboost_current_task()
+    print(fct.function_result_status.get_status_dict())
+    print(fct.function_result_status.task_id)
+    print(fct.function_result_status.run_times)
+    print(fct.full_msg)
+    '''
+import random
+import time
+
+from funboost import boost, FunctionResultStatusPersistanceConfig,BoosterParams
+from funboost.core.current_task import funboost_current_task
+
+@boost(BoosterParams(queue_name='queue_test_f01', qps=2,concurrent_num=5,
+       function_result_status_persistance_conf=FunctionResultStatusPersistanceConfig(
+           is_save_status=True, is_save_result=True, expire_seconds=7 * 24 * 3600)))
+def f(a, b):
+    fct = funboost_current_task()
+    print(fct.function_result_status.get_status_dict())
+    print(fct.function_result_status.task_id)
+    print(fct.function_result_status.run_times)
+    print(fct.full_msg)
+
+    time.sleep(20)
+    if random.random() > 0.5:
+        raise Exception(f'{a} {b} 模拟出错啦')
+    print(a+b)
+
+    return a + b
+
+
+if __name__ == '__main__':
+    # f(5, 6)  # 可以直接调用
+
+    for i in range(0, 200):
+        f.push(i, b=i * 2)
+
+    f.consume()
+
+    """
     _local_data = threading.local()
 
     @property
@@ -72,3 +116,10 @@ class AsyncioCurrentTask:
 
 def funboost_current_task():
     return AsyncioCurrentTask() if is_asyncio_environment() else ThreadCurrentTask()
+
+
+if __name__ == '__main__':
+    print()
+    for i in range(100000):
+        funboost_current_task()
+    print()

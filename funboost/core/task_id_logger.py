13c093aac8fddc4608f10438b5d5f077997e7082
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-03-21 16:13:49 +0800

    commit

--- funboost/core/task_id_logger.py
+++ funboost/core/task_id_logger.py
@@ -5,9 +5,15 @@ from funboost.core.current_task import get_current_taskid
 
 
 class TaskIdLogger(CompatibleLogger):
+    """
+    如果你要使用带taskid的日志模板,一定要使用
+     LogManager('namexx',logger_cls=TaskIdLogger).get_logger_and_add_handlers(....)
+     的方式来创建logger, 就是需要指定logger_cls=TaskIdLogger
+     """
     def _log(self, level, msg, args, exc_info=None, extra=None, stack_info=False):
         extra = extra or {}
         if 'task_id' not in extra:
             extra['task_id'] = get_current_taskid()
-        extra['sys_getframe_n'] = 3
+        if 'sys_getframe_n' not in extra:
+            extra['sys_getframe_n'] = 3
         super()._log(level, msg, args, exc_info, extra, stack_info)
--- test_frame/test_funboost_current_task/test_current_task.py
+++ test_frame/test_funboost_current_task/test_current_task.py
@@ -25,7 +25,7 @@ def f(a, b):
     logger.info(fct.function_result_status.task_id) # 获取消息的任务id
     logger.debug(fct.function_result_status.run_times) # 获取消息是第几次重试运行
     logger.info(fct.full_msg) # 获取消息的完全体。出了a和b的值意外，还有发布时间 task_id等。
-    logger.debug(fct.function_result_status.publish_time) # 获取消息的发布时间
+    logger.debug(fct.function_result_status.publish_time_str) # 获取消息的发布时间
     logger.debug(fct.function_result_status.get_status_dict()) # 获取任务的信息，可以转成字典看。
 
     time.sleep(2)

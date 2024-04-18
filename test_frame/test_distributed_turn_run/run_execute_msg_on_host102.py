6d015e4a1488d0c118ac4b18f4ee20df239f5e0d
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-04-18 17:01:04 +0800

    commit

--- test_frame/test_distributed_turn_run/run_execute_msg_on_host102.py
+++ test_frame/test_distributed_turn_run/run_execute_msg_on_host102.py
@@ -1,11 +1,11 @@
-from test_frame.test_distributed_turn_run.run_distribute_msg import build_booster_really_execute_msg_on_host_by_ip, ip_102
+from test_frame.test_distributed_turn_run.run_distribute_msg import build_booster_really_execute_msg_on_host_by_ip, ip_102,get_current_ip
 
 """
 现在是在单台机器模拟 实现  "两个机器轮流运行消息,并且同时只有一台机器在执行消息,同时只有一个消息被执行,不允许并发运行消息 " 这个需求,
 实际上是动态自动获取当前机器ip,不需要 run_execute_msg_on_host101.py run_execute_msg_on_host102.py 两个重复的文件
 """
 
-booster_execute_msg_on_host = build_booster_really_execute_msg_on_host_by_ip(ip_102)
+booster_execute_msg_on_host = build_booster_really_execute_msg_on_host_by_ip(get_current_ip(ip_102))
 
 if __name__ == '__main__':
     booster_execute_msg_on_host.fabric_deploy(0,0,0,0)

bd9eba9a16d35a7cccc07e07ef29623ac93ca9bf
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-11-28 01:22:06 +0800

    commit

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
         'decorator==5.1.1',
         'tomorrow3==1.1.0',
         'persist-queue>=0.4.2',
-        'apscheduler==3.10.1',
+        'apscheduler>=3.10.1',
         'pikav0',
         'pikav1',
         'redis2',

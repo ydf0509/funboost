09c6ad6ee0b6a75c4ee6f6ed14c5cdd64aa39b67
ydf0509 <909686719@qq.com>
2025-01-17 23:55:35 +0800

    9999

---
+++ test_frame/test_ais/test_sum_demo.py
@@ -0,0 +1,32 @@
+from funboost import boost, BrokerEnum,ctrl_c_recv,BoosterParams
+from funboost.timing_job.timing_push import ApsJobAdder  # 导入ApsJobAdder类
+
+
+# 定义任务处理函数
+@BoosterParams(queue_name='sum_queue3', broker_kind=BrokerEnum.REDIS)
+def sum_two_numbers(x, y):  
+    result = x + y 
+    print(f'The sum of {x} and {y} is {result}')  
+
+
+if __name__ == '__main__':
+    # 启动消费者
+    sum_two_numbers.consume()
+    
+    # 发布任务
+    sum_two_numbers.push(3, 5)
+    sum_two_numbers.push(10, 20)
+    
+    # 使用ApsJobAdder添加定时任务，
+    ApsJobAdder(sum_two_numbers, job_store_kind='redis').add_push_job(trigger='date',
+                                                                      run_date='2025-01-17 23:25:40', args=(7, 8))
+    
+    
+    ApsJobAdder(sum_two_numbers, job_store_kind='memory').add_push_job(trigger='interval', seconds=5, args=(4, 6))
+    
+    
+    ApsJobAdder(sum_two_numbers, job_store_kind='redis').add_push_job('cron', day_of_week='*', 
+                                                                      hour=23, minute=36, second=50, 
+                                                                      kwargs={"x":50,"y":60},replace_existing=True,id='cron_job1')
+    
+    ctrl_c_recv()
